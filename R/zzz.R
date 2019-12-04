420cee8b9045ca82fe2193a8d637f37bcd196b9f
Vince Carey <stvjc@channing.harvard.edu>
2019-12-04 07:20:30 -0500

    include import directive for setupVirtualEnv from basilisk

--- R/zzz.R
+++ R/zzz.R
@@ -1,3 +1,4 @@
+#' @importFrom basilisk setupVirtualEnv
 .onLoad <- function(libname, pkgname) {
     basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3", 
         "fasteners==0.15", "scandir==1.10.0", "monotonic==1.5", "numcodecs==0.6.4"), pkgname=pkgname)
