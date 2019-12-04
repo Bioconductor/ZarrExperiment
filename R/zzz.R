68437bf3d87233e169087a956658822b185531fd
Vince Carey <stvjc@channing.harvard.edu>
2019-12-03 19:30:20 -0500

    numcodecs version

--- R/zzz.R
+++ R/zzz.R
@@ -1,5 +1,5 @@
 .onLoad <- function(libname, pkgname) {
     basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3", 
-        "fasteners==0.15", "scandir==1.10.0", "monotonic==1.5"), pkgname=pkgname)
+        "fasteners==0.15", "scandir==1.10.0", "monotonic==1.5", "numcodecs==0.6.4"), pkgname=pkgname)
 }
 
