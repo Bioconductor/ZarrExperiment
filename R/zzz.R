98fbf764880e193420f1dc97a38e25652fd4b5c2
Vince Carey <stvjc@channing.harvard.edu>
2019-12-03 19:13:16 -0500

    various deps needed in python land

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,4 @@
 .onLoad <- function(libname, pkgname) {
-    basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3"), pkgname=pkgname)
+    basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3", "fasteners==0.15"), pkgname=pkgname)
 }
 
