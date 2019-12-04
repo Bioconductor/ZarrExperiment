9271e7aa3e7febda96b66c92210fab62c1d7ef16
Vince Carey <stvjc@channing.harvard.edu>
2019-12-03 19:23:50 -0500

    monotonic needed in python

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,5 @@
 .onLoad <- function(libname, pkgname) {
-    basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3", "fasteners==0.15", "scandir==1.10.0"), pkgname=pkgname)
+    basilisk::setupVirtualEnv("bczarrenv", c("zarr==2.3.2", "asciitree==0.3.3", 
+        "fasteners==0.15", "scandir==1.10.0", "monotonic==1.5"), pkgname=pkgname)
 }
 
